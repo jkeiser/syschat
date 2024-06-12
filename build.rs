6589da2955ee0a2d81920b123506f8f873ffb594
John Keiser <john@johnkeiser.com>
2024-06-12 11:06:57 -0700

    Add build.rs to build npm automatically

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+use std::path::Path;
+
+fn main() {
+    // Rerun npm install / npm run build if the client directory changes
+    Command::new("npm").arg("install")
+                       .current_dir(&Path::new("client"))
+                       .status().unwrap();
+    Command::new("npm").args(&["run", "build"])
+                       .current_dir(&Path::new("client"))
+                       .status().unwrap();
+
+    println!("cargo::rerun-if-changed=client");
+}
